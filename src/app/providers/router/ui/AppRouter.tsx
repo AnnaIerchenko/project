88c968bd9a49bdeb82180fe7c59fdda3af8187ef
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2023-02-08 08:10:17 -0800

    notfoundPage, pageLoader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -3,17 +3,18 @@ import { MainPage } from 'pages/MainPage'
 import React, { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { routeConfig } from 'shared/config/routeConfig/routeConfig'
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader'
 
 const AppRouter = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+  
       <Routes>
         {Object.values(routeConfig).map(({element, path}) => {
           <Route 
             key={path}
             path={path}
             element={(
-              <Suspense fallback={<div>Loading...</div>}>
+              <Suspense fallback={<PageLoader />}>
                 <div className='page-wrapper'>
 
                 {element}
@@ -23,7 +24,7 @@ const AppRouter = () => {
           />
         })}
       </Routes>
-  </Suspense>
+ 
   )
 }
 
